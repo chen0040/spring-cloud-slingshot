d0380216df945d317c191904a554613146cc17e9
xschen <xs0040@gmail.com>
2017-09-16 11:46:38 +0800

    fix issues with syslog4j server build

--- make.ps1
+++ make.ps1
@@ -9,9 +9,11 @@ if($args.length -gt 0) {
 
 Invoke-Expression -Command:"mvn -f pom.xml clean package -P$profile -U"
 
-$proj="spring-boot-slingshot"
-$source=$PSScriptRoot + "/target/" + $proj + ".jar"
-$dest=$PSScriptRoot + "/bin/" + $proj + ".jar"
-copy $source $dest
+$projs=@("sbs-eureka-app", "sbs-eureka-server", "sbs-mariadb-server", "sbs-eureka-web", "sbs-redis-server", "sbs-syslog4j-server")
+foreach ($proj in $projs){
+    $source=$PSScriptRoot + "/target/" + $proj + ".jar"
+    $dest=$PSScriptRoot + "/bin/" + $proj + ".jar"
+    copy $source $dest
+}
 
 cd $currentPath
