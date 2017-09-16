c6d3cee7cf306e70d4a29d75ab3ba2edbd02069b
xschen <xs0040@gmail.com>
2017-09-16 13:37:12 +0800

    fix eureka server crash

---
+++ status-cluster.ps1
@@ -0,0 +1,18 @@
+
+# ping sbs-eureka-web
+Invoke-WebRequest -URI http://localhost:8080/ping
+
+# ping sbs-eureka-app
+Invoke-WebRequest -URI http://localhost:9020/ping
+
+# ping sbs-eureka-server
+Invoke-WebRequest -URI http://localhost:8761/ping
+
+# ping sbs-redis-server
+Invoke-WebRequest -URI http://localhost:7379/ping
+
+# ping sbs-mariadb-server
+Invoke-WebRequest -URI http://localhost:3088/ping
+
+# ping sbs-syslog4j-server
+Invoke-WebRequest -URI http://localhost:8088/ping
